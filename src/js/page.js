a9e0e9a1925e7d9a0b039f3d80e923b608033443
tylerwalters <tyler@tylerwalters.com>
2016-02-24 15:09:52 -0700

    Refactoring code and adding form validation

--- src/js/page.js
+++ src/js/page.js
@@ -1,52 +1,130 @@
-import hp from './converter';
-import util from './utilities'
+import * as hp from './converter';
+import {formatNumber} from './utilities'
 
-export default (() => {
-  const _getValues = () => {
-    let vehicle = document.querySelector('[data-js=vehicle]');
-    let horsepower = document.querySelector('[data-js=horsepower]');
-    let type = document.querySelector('[data-js=type]:checked');
+const _vehicle    = document.querySelector('[data-js=vehicle]');
+const _horsepower = document.querySelector('[data-js=horsepower]');
+const _types      = document.querySelectorAll('[data-js=type]');
 
-    return {
-      vehicle: vehicle.value,
-      horsepower: horsepower.value,
-      type: type.value
+const _getSelectedType = (types) => {
+  const selected = [].filter.call(types, type => type.checked);
+
+  return selected[0] ? selected[0].value : '';
+};
+
+/**
+ *
+ * @param view
+ * @private
+ */
+const _changeView = (view) => {
+  const views = document.querySelectorAll('.view');
+  const activeSelector = document.querySelector('[data-js=' + view + ']');
+
+  for (let i = 0, length = views.length; i < length; i++) {
+    if (views[i].classList.contains('active')) {
+      views[i].classList.remove('active');
     }
-  };
-
-  const _changeView = (view) => {
-    let views = document.querySelectorAll('.view'),
-        activeSelector = document.querySelector('[data-js=' + view + ']');
-
-    for (let i = 0, length = views.length; i < length; i++) {
-      if (views[i].classList.contains('active')) {
-        views[i].classList.remove('active');
-      }
-    }
-
-    activeSelector.classList.add('active');
-  };
-
-  const _showResults = (vehicle, hp, conversion) => {
-    let results = document.querySelector('[data-js=results]'),
-        template = `<p>Your</p>
-                    <div class="display">${vehicle}</div>
-                    <p>has</p>
-                    <div class="display">${util.formatNumber(hp)} ${conversion.name} power</div>`;
-
-    results.innerHTML = template;
-    _changeView('results');
-  };
-
-  const actionSubmit = () => {
-    let userInput = _getValues(),
-        convertedPower = hp.convert(userInput.horsepower, userInput.type),
-        conversion = hp.getConversion(userInput.type);
-
-    _showResults(userInput.vehicle, convertedPower, conversion);
-  };
-
-  return {
-    actionSubmit: actionSubmit
   }
-})();
+
+  activeSelector.classList.add('active');
+};
+
+/**
+ *
+ * @param vehicle
+ * @param hp
+ * @param conversion
+ * @private
+ */
+const _showResults = (vehicle, hp, conversion) => {
+  const results = document.querySelector('[data-js=results]');
+  const template = `<a id="homeLink" data-js="home-link">Back Home</a>
+                  <h2>${conversion.name} Power</h2>
+                  <p>Your</p>
+                  <div class="display">${vehicle}</div>
+                  <p>has</p>
+                  <div class="display">${formatNumber(hp)} ${conversion.name} power</div>`;
+
+  let homeLink;
+
+  results.innerHTML = template;
+  _changeView('results');
+  _buildHomeLink();
+};
+
+const _buildHomeLink = () => {
+  const homeLink = document.querySelector('[data-js=home-link]');
+
+  homeLink.addEventListener('click', () => {
+    _changeView('home');
+  })
+};
+
+const _validateValues = (entry) => {
+  switch (entry.type) {
+    case 'vehicle':
+      return entry.value.length > 0;
+    case 'horsepower':
+      return entry.value > 0;
+    case 'type':
+      return entry.value.length > 0;
+  }
+};
+
+const _toggleError = (type, display = 'hide') => {
+  const error = document.querySelector('[data-js=error-' + type + ']');
+
+  switch (display) {
+    case 'show':
+      error.classList.remove('hidden');
+      break;
+    case 'hide':
+      error.classList.add('hidden');
+      break;
+  }
+};
+
+/**
+ *
+ */
+export const actionSubmit = () => {
+  const vehicle = _vehicle.value;
+  const horsepower = _horsepower.value;
+  const type = _getSelectedType(_types);
+
+  let errors = false,
+      convertedPower,
+      conversion;
+
+  if (_validateValues({type: 'vehicle', value: vehicle})) {
+    _toggleError('vehicle', 'hide');
+  } else {
+    errors = true;
+    _toggleError('vehicle', 'show');
+  }
+
+  if (_validateValues({type: 'horsepower', value: horsepower})) {
+    _toggleError('horsepower', 'hide');
+  } else {
+    errors = true;
+    _toggleError('horsepower', 'show');
+  }
+
+  if (_validateValues({type: 'type', value: type})) {
+    _toggleError('type', 'hide');
+  } else {
+    errors = true;
+    _toggleError('type', 'show');
+  }
+
+  if (errors) {
+    return false;
+  }
+
+  convertedPower = hp.convert(horsepower, type);
+  conversion = hp.getConversion(type);
+
+  _showResults(vehicle, convertedPower, conversion);
+};
+
+
