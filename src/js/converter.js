356c7a5f7254ad43907f9950dadf52a62d5149fb
tylerwalters <tyler@tylerwalters.com>
2016-02-24 15:09:31 -0700

    Refactoring code

--- src/js/converter.js
+++ src/js/converter.js
@@ -4,50 +4,42 @@
 // Tyrannosaurus Rex: weight - 17990lbs, top speed - 18mph, horsepower (trap speed formula): 8.1884387801548 (http://www.livescience.com/23868-tyrannosaurus-rex-facts.html)
 // Tortoise: weight - 661.4lb, top speed - 0.2mph, horsepower (trap speed formula): 4.1295868602465E-7 (http://www.speedofanimals.com/animals/galapagos_tortoise)
 
-export default (() => {
-  const _conversions = {
-    trex: {
-      name: 'Tyrannosaurus Rex',
-      horsepower: 8.1884387801548,
-      source: 'http://www.livescience.com/23868-tyrannosaurus-rex-facts.html'
-    },
-    tortoise: {
-      name: 'Tortoise',
-      horsepower: 4.1295868602465E-7,
-      source: 'http://www.speedofanimals.com/animals/galapagos_tortoise'
-    },
-    spaceShuttle: {
-      name: 'Space Shuttle',
-      horsepower: 37000000,
-      source: 'http://www.nasa.gov/returntoflight/system/system_SSME.html'
-    }
-  };
-
-  /**
-   *
-   * @param type
-   * @returns {*}
-   */
-  const getConversion = (type) => _conversions[type];
-
-  /**
-   *
-   * @param weight
-   * @param velocity
-   */
-  const trapSpeed = (weight, velocity) => weight * (velocity/234)^3;
-
-  /**
-   *
-   * @param hp
-   * @param type
-   * @returns {number}
-   */
-  const convert = (hp, type) => parseInt(hp) / _conversions[type].horsepower;
-
-  return {
-    convert: convert,
-    getConversion: getConversion,
-    trapSpeed: trapSpeed
+const _conversions = {
+  trex: {
+    name: 'Tyrannosaurus Rex',
+    horsepower: 8.1884387801548,
+    source: 'http://www.livescience.com/23868-tyrannosaurus-rex-facts.html'
+  },
+  tortoise: {
+    name: 'Tortoise',
+    horsepower: 4.1295868602465E-7,
+    source: 'http://www.speedofanimals.com/animals/galapagos_tortoise'
+  },
+  spaceShuttle: {
+    name: 'Space Shuttle',
+    horsepower: 37000000,
+    source: 'http://www.nasa.gov/returntoflight/system/system_SSME.html'
   }
-})();
+};
+
+/**
+ *
+ * @param type
+ * @returns {*}
+ */
+export const getConversion = (type) => _conversions[type];
+
+/**
+ *
+ * @param weight
+ * @param velocity
+ */
+export const trapSpeed = (weight, velocity) => weight * (velocity/234)^3;
+
+/**
+ *
+ * @param hp
+ * @param type
+ * @returns {number}
+ */
+export const convert = (hp, type) => parseInt(hp) / _conversions[type].horsepower;
--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,18 +1,12 @@
-export default (() => {
-  /**
-   *
-   * @param num
-   * @returns {*}
-   */
-  const formatNumber = (num) => {
-    if (num > 1) {
-      return num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    } else {
-      return num;
-    }
-  };
-
-  return {
-    formatNumber: formatNumber
+/**
+ *
+ * @param num
+ * @returns {*}
+ */
+export const formatNumber = (num) => {
+  if (num > 1) {
+    return num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  } else {
+    return num;
   }
-})();
+};
